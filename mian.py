b91137fb7569ce2c65a4522cbc9de54acbb3e035
Treyvon Pearson <tpearson3@my.waketech.edu>
2025-11-21 12:26:05 -0500

    1.0

---
+++ mian.py
@@ -0,0 +1,14 @@
+import time
+print("Is Diddy blud on the calculator?")
+decision = input("yes or no:")
+decision = decision.lower()
+if decision == "yes":
+    for i in range(2500):
+        time.sleep(0.67)
+        print("What is this Diddy blud doing on the calculator")
+        time.sleep(0.2)
+        print("Is Blud Einstein")
+elif decision == "no":
+    print("Diddy Blud is NOT on the calculator")
+else:
+    print("Invalid Diddy Blud")
